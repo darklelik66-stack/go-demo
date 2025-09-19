4940a79c8a61809837b33c8f471ccee8d0fd6b6e
a.marchenko <a.marchenko@bft.ru>
2025-09-19 12:01:24 +0300

    Refactor currency conversion logic in main.go by renaming kursRUB to kursRUB_USD and updating the RUB to EUR conversion calculation for improved accuracy.

--- main.go
+++ main.go
@@ -5,16 +5,16 @@ import (
 )
 
 func main() {
-	// TODO: use constant for kursUSD and kursEUR
+
 	const kursUSD = 0.8503
 	const kursEUR = 1.18
-	const kursRUB = 98.98
+	const kursRUB_USD = 98.98
 
 	var amountUSD float64 = 100
 	var amountEUR float64 = 100
 	var resultUSD float64 = amountEUR * kursUSD
 	var resultEUR float64 = amountUSD * kursEUR
-	var resultRUBEUR float64 = amountEUR * kursRUB
+	var resultRUBEUR float64 = amountEUR * (kursRUB_USD / kursEUR)
 
 	fmt.Println(resultUSD)
 	fmt.Println(resultEUR)
