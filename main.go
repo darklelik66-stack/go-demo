3720fe719205b774e0f5239375b134d668f8cbf2
a.marchenko <a.marchenko@bft.ru>
2025-09-19 11:37:26 +0300

    Initialize Go module and add main application code with currency conversion logic. Also, include .gitignore file to exclude app-1 directory.

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const kursUSD = 0.8503
+	const kursEUR = 1.18
+
+	var amountUSD float64 = 100
+	var amountEUR float64 = 100
+	var amountUSD float64 = amountEUR * kursUSD
+	var amountEUR float64 = amountUSD * kursEUR
+
+	fmt.Println(amountUSD)
+	fmt.Println(amountEUR)
+
+}
