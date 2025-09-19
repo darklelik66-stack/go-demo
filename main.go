8d60eaba92aad4b50bf72e57e6a36901079bec69
a.marchenko <a.marchenko@bft.ru>
2025-09-19 11:56:00 +0300

    Refactor currency conversion variables in main.go to improve clarity by renaming amount variables to result variables.

--- main.go
+++ main.go
@@ -8,13 +8,15 @@ func main() {
 	// TODO: use constant for kursUSD and kursEUR
 	const kursUSD = 0.8503
 	const kursEUR = 1.18
+	const kursRUB = 98.98
 
 	var amountUSD float64 = 100
 	var amountEUR float64 = 100
-	var amountUSD float64 = amountEUR * kursUSD
-	var amountEUR float64 = amountUSD * kursEUR
+	var resultUSD float64 = amountEUR * kursUSD
+	var resultEUR float64 = amountUSD * kursEUR
+	var resultRUBEUR float64 = amountEUR * kursRUB
 
-	fmt.Println(amountUSD)
-	fmt.Println(amountEUR)
-
+	fmt.Println(resultUSD)
+	fmt.Println(resultEUR)
+	fmt.Println(resultRUBEUR)
 }
