4dde7d033d5a45e5688674100c4d86563dbc3f0a
a.marchenko <a.marchenko@bft.ru>
2025-09-20 20:18:01 +0300

    Move currency rate constants to the top of main.go and implement getUserСurrency function for user input, enhancing code organization and functionality.

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 )
 
+const kursUSD = 0.8503
+const kursEUR = 1.18
+const kursRUB_USD = 98.98
+
 func main() {
 
-	const kursUSD = 0.8503
-	const kursEUR = 1.18
-	const kursRUB_USD = 98.98
+	getUserСurrency()
 
 	var amountUSD float64 = 100
 	var amountEUR float64 = 100
@@ -19,4 +21,15 @@ func main() {
 	fmt.Println(resultUSD)
 	fmt.Println(resultEUR)
 	fmt.Println(resultRUBEUR)
+}
+
+func getUserСurrency() float64 {
+	var ammountCurrency float64
+	fmt.Println("Введите сумму валюты:")
+	fmt.Scan(&ammountCurrency)
+	return ammountCurrency
+}
+
+func calculateCurrency(ammountCurrency float64, initialCurrency string, finalCurrency string) float64 {
+
 }
